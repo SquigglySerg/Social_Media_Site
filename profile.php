56547a0f0ad417eac201529ce6a45e480f66e3e8
SquigglySerg <serodrig@mymail.mines.edu>
2017-12-12 19:34:30 -0700

    The's stuff

--- profile.php
+++ profile.php
@@ -9,12 +9,63 @@
 		<link rel="stylesheet" href="style.css">
 		<title>Modern Peeps</title>
 	</head>
+	<script>
+		function changeColor() {
+			if(document.getElementById('background_colors').value == "cyan") {
+                        	document.getElementById("profile_info").style = "background-color: cyan";
+                	}   	
+			else if(document.getElementById('background_colors').value == "yellow") {
+                        	document.getElementById("profile_info").style = "background-color: yellow";
+                	}   	
+			else if(document.getElementById('background_colors').value == "red") {
+                        	document.getElementById("profile_info").style = "background-color: red";
+                	}   	
+		}
+	
+		function changeImage() {
+			if(document.getElementById('images').value == "cat") {
+                        	document.getElementById("img").src = "images/cat.jpg";
+                	}   	
+			else if(document.getElementById('images').value == "dog") {
+                        	document.getElementById("img").src = "images/dog.jpg";
+                	}   	
+			else if(document.getElementById('images').value == "turtle") {
+                        	document.getElementById("img").src = "images/turtle.jpg";
+                	}   	
+		}
+	</script>
 	<body>
 		<div id="profile_info">
+			<div id="profile_image">
+			<img id="img" src="" alt="#">
+			</div>
+			
 			<div id="intro">
 			Introduction
 			</div>		
-			<div id="profile_image">
+			
+			<div id="hobbies">
+			Hobbies
+			</div>
+			
+			<div id="music">
+			Music
+			</div>
+			
+			<div id="selectors">
+				<select id="images" onChange="changeImage()">
+					<option value="none">Select an Avatar Image</option>
+					<option value="cat">Cat</option>
+					<option value="dog">Dog</option>
+					<option value="turtle">Turtle</option>
+				</select>
+			
+				<select id="background_colors" onChange="changeColor()">
+					<option value="none">Select a Background Color</option>
+					<option value="cyan">Cyan</option>
+					<option value="yellow">Yellow</option>
+					<option value="red">Red</option>
+				</select>
 			</div>
 		</div>
 		
