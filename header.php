09ca7f911460feee1920a270c32bc3ca71cebe08
SquigglySerg <serodrig@mymail.mines.edu>
2017-12-13 18:30:21 -0700

    border changes to links in the nav bar

--- header.php
+++ header.php
@@ -36,7 +36,10 @@ ul.topnav li a:hover:not(.active) {background-color: #111;}
 
 ul.topnav li a.active {background-color: #4CAF50;}
 
-ul.topnav li.right {float: right;}
+ul.topnav li.right {
+	float: right;
+	border-left: 1px solid #bbb;
+}
 
 @media screen and (max-width: 600px){
     ul.topnav li.right, 
