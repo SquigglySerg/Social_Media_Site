a296ec55ab0899df2ad2734bfda1e47de02bff58
SquigglySerg <serodrig@mymail.mines.edu>
2017-12-13 20:22:01 -0700

    nav bar now changes on the login status

--- header.php
+++ header.php
@@ -49,7 +49,28 @@ ul.topnav li.right {
 <div class="sitename"><h1>Modern Peeps Non-Dating Site</h1></div>
 <ul class="topnav">
   <li><a href="./index.php" <?php if($active == "index"){echo "class=\"active\"";} ?>>Home</a></li>
-  <li><a href="./profile.php" <?php if($active == "profile"){echo "class=\"active\"";} ?>>Profile</a></li>
-  <li class="right"><a href="./register.php" <?php if($active == "register"){echo "class=\"active\"";} ?>>Sign Up</a></li>
-  <li class="right"><a href="./login.php" <?php if($active == "login"){echo "class=\"active\"";} ?>>Login</a></li>
+  
+  <?php
+	//Check if a session has started
+    if (session_status() == PHP_SESSION_NONE) {
+		session_start();
+	}
+	
+	if(isset($_SESSION['email']) && !empty($_SESSION['email']) ){
+		//User logged in
+		if($active == "profile"){echo "<li><a href=\"./profile.php\" class=\"active\" >Profile</a></li>";}
+		else {echo "<li><a href=\"./profile.php\">Profile</a></li>";}
+		
+		echo "<li class=\"right\"><a href=\"./login.php\">Logout</a></li>";
+	}
+	else{
+		//User not logged in
+		if($active == "register"){echo "<li class=\"right\"><a href=\"./register.php\" class=\"active\">Sign Up</a></li>";}
+		else{echo "<li class=\"right\"><a href=\"./register.php\">Sign Up</a></li>";}
+		
+		if($active == "login"){echo "<li class=\"right\"><a href=\"./login.php\" class=\"active\">Login</a></li>";}
+		else {echo "<li class=\"right\"><a href=\"./login.php\">Login</a></li>";}
+	}
+  ?>
+  
 </ul>
